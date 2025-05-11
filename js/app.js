9dac12b688f6be6e6b1638347e993d4858c42e14
Homen que programa <168280467+Homen-que-programa@users.noreply.github.com>
2025-05-11 13:43:06 -0300

    Delete js/app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById('canvas')
-
-function resizeCanvas() {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-}
-
-window.addEventListener('resize', resizeCanvas)
-resizeCanvas()
